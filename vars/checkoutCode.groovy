51fce2e9e8f9df161e2e9d8f9e764178be97cd9c
roshniv6 <roshniaishu6@gmail.com>
2024-02-22 17:21:52 +0530

    shared library

--- vars/checkoutCode.groovy
+++
@@ -1,6 +0,0 @@
-def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
-   sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
-   return workingDir
-}
---
+++ vars/helloWorld.groovy
@@ -0,0 +1,6 @@
+// vars/helloWorld.groovy
+
+def call() {
+    echo 'Hello, world!'
+}
+
